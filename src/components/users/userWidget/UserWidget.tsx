9857add65a11a2c332d8e04ff2f0a96e67bb5092
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-04-19 09:37:04 +0330

    feat: add userwidget component

---
+++ src/components/users/userWidget/UserWidget.tsx
@@ -0,0 +1,21 @@
+import { List, ListItem, Divider, ListItemText, ListItemAvatar, Avatar } from '@mui/material'
+
+import { UserType } from '../../../type/UserType'
+
+export default function UserWidget({ user }: { user: UserType }) {
+  return (
+    <>
+      <div>
+        <List>
+          <Divider variant="inset" />
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar alt={user.firstName} src={user.image} />
+            </ListItemAvatar>
+            <ListItemText primary={user.firstName} secondary={user.email} />
+          </ListItem>
+        </List>
+      </div>
+    </>
+  )
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,17 @@
 
 import HomeCard from "../../components/home/homeCard/HomeCard";
 import Chart from "../../components/global/chart/Chart";
-import { HomeCardData, HomeChartData } from "../../assets/data/Data";
+import UserWidget from "../../components/users/userWidget/UserWidget";
+import { HomeCardData, HomeChartData, UserData } from "../../assets/data/Data";
 
 export default function Home ():React.JSX.Element {
   return(
     <>
-    
+    {
+      UserData.slice(0,3).map(user =>(
+        <UserWidget key={user.id} user={user}/>
+      ))
+    }
     </>
   )
 }
