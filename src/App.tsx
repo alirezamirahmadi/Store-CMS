d951e17f5822a70284158d4d20e630fb301c2f32
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-05-23 07:20:51 +0330

    fix: show login page when reload page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRoutes } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useCookies } from "react-cookie";
@@ -9,16 +9,23 @@ import routes from "./route/Route";
 import { getLogin } from "./redux/reducers/LoginReducer";
 import '../dist/tailwind/output.css';
 import Login from "./pages/login/Login";
+import Loading from "./components/global/loading/Loading";
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const dispatch = useDispatch<AppDispatch>();
   const loginInfo = useSelector((state: RootState) => state.login);
   const [cookies, ,] = useCookies(['token']);
   const router = useRoutes(routes(loginInfo.isLogin));
 
   useEffect(() => {
-    dispatch(getLogin(cookies.token));
+    dispatch(getLogin(cookies.token)).then(() => setIsLoading(false));
   }, [])
+
+  if (isLoading) {
+    return (<div className="mt-20"><Loading /></div>)
+  }
 
   return (
     <>
